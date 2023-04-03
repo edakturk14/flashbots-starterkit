f7470539dacab6772bd0e544bf47dd8adaec1169
Eda Akturk <edaakturk@192.168.1.120>
2023-04-03 12:08:33 +0300

    Update frontend

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -13,21 +13,32 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">Built w/Scaffold-eth</span>
-            <span className="block text-4xl font-bold">Flashbots StarterKit</span>
+            <span className="block text-4xl font-bold">🤖 Flashbots StarterKit</span>
           </h1>
-          <div className="flex-col gap-2 items-center">
+          <div className="flex flex-col gap-2 items-center">
             <span className="font-bold italic text-gray-500 text-lg">Transaction#1</span>{" "}
             <input
-              className="input mb-2 focus:bg-transparent focus:text-gray-400 px-4 w-full font-medium placeholder:text-accent/50 text-gray-400 border-2 border-base-300 bg-base-200 rounded-full"
+              className="input focus:bg-transparent focus:text-gray-400 px-4 w-full font-medium placeholder:text-accent/50 text-gray-400 border-2 border-base-300 bg-base-200 rounded-full"
               type="text"
               placeholder="Amount"
             />
             <input
-              className="input mb-2 focus:bg-transparent focus:text-gray-400 px-4 w-full font-medium placeholder:text-accent/50 text-gray-400 border-2 border-base-300 bg-base-200 rounded-full"
+              className="input focus:bg-transparent focus:text-gray-400 px-4 w-full font-medium placeholder:text-accent/50 text-gray-400 border-2 border-base-300 bg-base-200 rounded-full"
               type="text"
               placeholder="Account"
             />
-            <button className="btn btn-secondary btn-sm bg-base-300 w-full">Send transaction</button>
+            <span className="font-bold italic text-gray-500 text-lg">Transaction#2</span>{" "}
+            <input
+              className="input focus:bg-transparent focus:text-gray-400 px-4 w-full font-medium placeholder:text-accent/50 text-gray-400 border-2 border-base-300 bg-base-200 rounded-full"
+              type="text"
+              placeholder="Amount"
+            />
+            <input
+              className="input focus:bg-transparent focus:text-gray-400 px-4 w-full font-medium placeholder:text-accent/50 text-gray-400 border-2 border-base-300 bg-base-200 rounded-full"
+              type="text"
+              placeholder="Account"
+            />
+            <button className="btn btn-primary btn-sm bg-base-300 w-full">Bundle and Send transactions</button>
           </div>
         </div>
       </div>
